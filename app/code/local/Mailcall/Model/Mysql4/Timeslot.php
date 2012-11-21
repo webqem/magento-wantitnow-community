2840d02cd4f40e4cd70867ceaeef34aeccf01760
hoadx <hoadx@192.168.0.55>
2012-11-21 23:44:10 +0700

    - Initial project

--- app/code/local/Mailcall/Model/Mysql4/Timeslot.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Webqem_Mailcall_Model_Mysql4_Timeslot extends Mage_Core_Model_Mysql4_Abstract
-{
-    public function _construct()
-    {
-        $this->_init('webqemmailcall/timeslot', 'timeslot_id');
-    }
-}
